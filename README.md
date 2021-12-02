29ed8ef541693946b3d2e6510de84b9a2419dcf0
PedroSilva201 <13pedro.tome@gmail.com>
2021-12-02 08:29:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-b2rtwh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-b2rtwh)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Seja bem vindo ao  StackBlitz!</h1>
+      <p>Começe a editar programas usando linguagem de alto padrao</p>
+    </div>
+  );
+}
